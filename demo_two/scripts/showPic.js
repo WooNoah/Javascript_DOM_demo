8b953826dec937886b855eb3bb6c78796a7b9d15
WooNoah <wwdu2010@gmail.com>
2017-09-16 14:52:52 +0800

    动态操作DOM

--- demo_two/scripts/showPic.js
+++ demo_two/scripts/showPic.js
@@ -1,15 +1,25 @@
 		function showPic(whichpic) {
-			var picSource = whichpic.getAttribute("href");
+			if (!document.getElementById) return false;
+			if (!document.getElementById("placeholder")) return false;
+			if (!document.getElementById("description")) return false;
+
+
+			var source = whichpic.getAttribute("href");
 			var placeholder = document.getElementById("placeholder");
+			placeholder.setAttribute("src", source);
 
+			if (whichpic.getAttribute("title")) {
+				var text = whichpic.getAttribute("title");
+			}else {
+				var text = "";
+			}
 
-			placeholder.setAttribute("src", picSource);
-
-			var title = whichpic.getAttribute("title");
 			var description = document.getElementById('description');
 			// description.innerHTML = title;
-			description.firstChild.nodeValue = title;
-
+			if (description.firstChild.nodeType == 3) {
+				description.firstChild.nodeValue = text;
+			}
+			return false;
 			// console.log(description.childNodes.nodeValue);
 			// console.log(description.firstChild);
 			// console.log(description.lastChild);
@@ -24,6 +34,63 @@
 			alert(body_element.childNodes.length);
 		}
 
+		/**
+		 *	动态创建预览图片和文字
+		 *	html中注释掉的<img>和<p>
+		 */
+		function preparePlaceholder() {
+			var placeholder = document.createElement('img');
+			placeholder.setAttribute("id","placeholder");
+			placeholder.setAttribute("src", "#");
+			placeholder.setAttribute("alt", "My image Gallery");
+			var description = document.createElement('p');
+			description.setAttribute("id", "description");
+
+			var desctext = document.createTextNode("Choose an image");
+			description.appendChild(desctext);
+
+			//添加到body中<append方法默认会添加到最后边>
+			document.getElementsByTagName("body")[0].appendChild(placeholder);
+			document.getElementsByTagName("body")[0].appendChild(description);
+
+
+			//也可以添加到前边 insertBefore
+			var ulElement = document.getElementById("imagegallery");
+			ulElement.parentNode.insertBefore(placeholder, ulElement);
+			ulElement.parentNode.insertBefore(description, ulElement);
+		}
+
+		//有insertBefore方法，理论上就应该有insertAfter,但是，DOM中没有提供此方法，jQuery中有的。
+		//这里自己实现一个insertAfter函数
+		function insertAfter(newElement, targetElement) {
+			var parent = targetElement.parentNode;
+			if (parent.lastChild == targetElement) {
+				 parent.append(newElement);
+			}else {
+				 parent.insertBefore(newElement, targetElement.nextSubling);
+			}
+		}
+
+		/**
+		 *	使用上面的insertAfter来动态创建预览图片和文字
+		 */
+		function useInsertAfterToPreparePlaceholder() {
+			//前边创建DOM一样
+			var placeholder = document.createElement('img');
+			placeholder.setAttribute("id","placeholder");
+			placeholder.setAttribute("src", "#");
+			placeholder.setAttribute("alt", "My image Gallery");
+			var description = document.createElement('p');
+			description.setAttribute("id", "description");
+			var desctext = document.createTextNode("Choose an image");
+			description.appendChild(desctext);
+
+
+			var ul = document.getElementById("imagegallery");
+			insertAfter(placeholder, ul);
+			insertAfter(description, placeholder);
+		}
+
 
 		function prepareGallery() {
 			if (!document.getElementById) return false;
@@ -34,9 +101,9 @@
 			var links = gallery.getElementsByTagName("a");
 			for (var i = 0; i < links.length; i++) {
 				links[i].onclick = function() {
-					showPic(this);
-					return false;
+					return showPic(this);
 				}
+				links[i].onkeypress = links[i].inclick;
 			}
 		}
 
@@ -53,6 +120,13 @@
 			}
 		}
 
+		//使用上面的insertAfter来动态创建预览图片和文字
+		// addLoadEvent(useInsertAfterToPreparePlaceholder);
 
+		//展示图片和描述在上
+		addLoadEvent(preparePlaceholder);
+		//初始化a标签点击事件
 		addLoadEvent(prepareGallery);
+
+
 		// window.onload = countBodyChildren;
