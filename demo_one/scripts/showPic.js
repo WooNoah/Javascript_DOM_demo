8f6cde6f651b9ee9a19fe779a9efcc1dc0b15954
WooNoah <wwdu2010@gmail.com>
2017-09-16 13:32:33 +0800

    优化onload方法,修改a点击事件触发处

--- demo_one/scripts/showPic.js
+++ demo_one/scripts/showPic.js
@@ -1,23 +1,58 @@
 		function showPic(whichpic) {
-			var placeholder = document.getElementById("placeholder");
 			var picSource = whichpic.getAttribute("href");
+			var placeholder = document.getElementById("placeholder");
+
 
 			placeholder.setAttribute("src", picSource);
 
 			var title = whichpic.getAttribute("title");
 			var description = document.getElementById('description');
-			// description.innerHTML = title;	
-			description.firstChild.nodeValue = title;	
+			// description.innerHTML = title;
+			description.firstChild.nodeValue = title;
 
 			// console.log(description.childNodes.nodeValue);
 			// console.log(description.firstChild);
-			// console.log(description.lastChild);	
+			// console.log(description.lastChild);
 		}
 
 
+		/**
+		 * 	test
+		 */
 		function countBodyChildren() {
 			var body_element = document.getElementsByTagName('body')[0];
-			// alert(body_element.childNodes.length);
+			alert(body_element.childNodes.length);
 		}
 
-		window.onload = countBodyChildren;
+
+		function prepareGallery() {
+			if (!document.getElementById) return false;
+			if (!document.getElementsByTagName) return false;
+			if (!document.getElementById("imagegallery")) return false;
+
+			var gallery = document.getElementById('imagegallery');
+			var links = gallery.getElementsByTagName("a");
+			for (var i = 0; i < links.length; i++) {
+				links[i].onclick = function() {
+					showPic(this);
+					return false;
+				}
+			}
+		}
+
+
+		function addLoadEvent(func) {
+			var oldonload = window.onload;
+			if (typeof window.onload != 'function') {
+				window.onload = func;
+			}else {
+				window.onload = function() {
+					oldonload();
+					func();
+				}
+			}
+		}
+
+
+		addLoadEvent(prepareGallery);
+		// window.onload = countBodyChildren;
